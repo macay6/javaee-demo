20218ad6f2615379ba1b0484209562a1c3ba605b
macay <1207260521@qq.com>
2021-02-10 19:51:23 +0800

    实现Runnable接口创建线程

--- java-juc/src/main/java/creatThread/MyRunnableTest.java
+++ java-juc/src/main/java/creatThread/MyRunnableTest.java
@@ -24,17 +24,18 @@ public class MyRunnableTest {
             }
         }
     }
+}
 
-    static class MyRunnable2 implements Runnable {
-        public void run() {
-            for (int i = 0; i < 5; i++) {
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println(Thread.currentThread().getName() + "-------第" + i + "次执行");
+class MyRunnable2 implements Runnable {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+            System.out.println(Thread.currentThread().getName() + "-------第" + i + "次执行");
         }
     }
 }
+
