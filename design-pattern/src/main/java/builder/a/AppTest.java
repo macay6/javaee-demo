65f95e2b4eb16749149dcf5bdf8bd92b91c6e574
macay <1207260521@qq.com>
2023-04-28 22:02:27 +0800

    建造者模式

--- design-pattern/src/main/java/builder/a/AppTest.java
+++ design-pattern/src/main/java/builder/a/AppTest.java
@@ -7,6 +7,7 @@ package builder.a;
  * @Date: 2022/7/31 3:20 下午
  */
 public class AppTest {
+
     public static void main(String[] args) {
         ComputerBuilder computerBuilder = new ComputerBuilder();
         Computer computer = computerBuilder.builder();
