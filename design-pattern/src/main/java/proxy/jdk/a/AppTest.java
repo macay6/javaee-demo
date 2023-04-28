541b27d4b2995c7d192a3a9ac61e43dfa5c75455
macay <1207260521@qq.com>
2023-04-28 23:04:50 +0800

    jdk动态代理模式

--- design-pattern/src/main/java/proxy/jdk/a/AppTest.java
+++ design-pattern/src/main/java/proxy/jdk/a/AppTest.java
@@ -7,6 +7,7 @@ package proxy.jdk.a;
  * @Date: 2022/9/17 1:48 下午
  */
 public class AppTest {
+
     public static void main(String[] args) {
         ICalcImpl iCalc = new ICalcImpl();
         System.out.println(iCalc.add(1, 2));
