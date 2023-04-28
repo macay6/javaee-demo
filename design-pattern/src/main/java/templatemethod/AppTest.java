9cda9b40bcf6fd501b8366a7ef84d2fb60187d4c
macay <1207260521@qq.com>
2023-04-28 23:18:19 +0800

    模版方法模式

--- design-pattern/src/main/java/templatemethod/AppTest.java
+++ design-pattern/src/main/java/templatemethod/AppTest.java
@@ -7,6 +7,7 @@ package templatemethod;
  * @Date: 2022/8/3 10:08 下午
  */
 public class AppTest {
+
     public static void main(String[] args) {
         Cook_egg cook_egg = new Cook_egg();
         cook_egg.cookProcess();
