94c7f2c79c0528676068b3034fd71f8d96e87047
macay <1207260521@qq.com>
2023-04-28 22:25:10 +0800

    观察者模式

--- design-pattern/src/main/java/observer/a/AppTest.java
+++ design-pattern/src/main/java/observer/a/AppTest.java
@@ -7,6 +7,7 @@ package observer.a;
  * @Date: 2022/9/22 5:11 下午
  */
 public class AppTest {
+
     public static void main(String[] args) {
         Role role = new Role();
         role.setHp(100);
