27276002fd6b9453eecb1582a29cee009eada1a4
macay <1207260521@qq.com>
2023-04-28 23:17:54 +0800

    策略模式

--- design-pattern/src/main/java/strategy/a/AppTest.java
+++ design-pattern/src/main/java/strategy/a/AppTest.java
@@ -7,6 +7,7 @@ package strategy.a;
  * @Date: 2022/8/7 10:25 上午
  */
 public class AppTest {
+
     public static void main(String[] args) {
         RubberDuck rubberDuck = new RubberDuck();
         rubberDuck.quack();
