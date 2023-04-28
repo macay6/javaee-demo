bce1138f2da62ef747f2bbd227fa296cf2cd8f33
macay <1207260521@qq.com>
2023-04-28 23:04:27 +0800

    静态代理模式

--- design-pattern/src/main/java/proxy/statics/AppTest.java
+++ design-pattern/src/main/java/proxy/statics/AppTest.java
@@ -7,6 +7,7 @@ package proxy.statics;
  * @Date: 2022/9/17 10:44 上午
  */
 public class AppTest {
+
     public static void main(String[] args) {
         ProxyPoint proxyPoint = new ProxyPoint();
         proxyPoint.sell();
