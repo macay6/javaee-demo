9a74ddef95f0100ba08cd9abd8888ce7cfc6e883
macay <1207260521@qq.com>
2023-04-28 23:03:59 +0800

    cglib代理模式

--- design-pattern/src/main/java/proxy/cglib/AppTest.java
+++ design-pattern/src/main/java/proxy/cglib/AppTest.java
@@ -7,6 +7,7 @@ package proxy.cglib;
  * @Date: 2022/9/24 12:04 下午
  */
 public class AppTest {
+
     public static void main(String[] args) {
         IcePeak proxy = (IcePeak)CglibProxy.getProxy(IcePeak.class);
         proxy.drink();
