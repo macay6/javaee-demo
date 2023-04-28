a2b3e8d355a608d15718344b2d192b58dce4872e
macay <1207260521@qq.com>
2023-04-28 22:29:12 +0800

    依赖倒置原则

--- design-pattern/src/main/java/principle/dependenceInversionPrinciple/AMDCpu.java
+++ design-pattern/src/main/java/principle/dependenceInversionPrinciple/AMDCpu.java
@@ -7,6 +7,7 @@ package principle.dependenceInversionPrinciple;
  * @Date: 2022/7/24 4:31 下午
  */
 public class AMDCpu implements Cpu {
+
     @Override
     public void run() {
         System.out.println("AMDCpu跑起来了");
