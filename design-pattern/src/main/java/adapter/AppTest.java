76b3fcbc35fb2aae58b082890aac585033b7d1d6
macay <1207260521@qq.com>
2023-04-28 22:01:41 +0800

    适配器模式

--- design-pattern/src/main/java/adapter/AppTest.java
+++ design-pattern/src/main/java/adapter/AppTest.java
@@ -7,6 +7,7 @@ package adapter;
  * @Date: 2022/8/28 11:20 上午
  */
 public class AppTest {
+
     public static void main(String[] args) {
         Computer computer = new Computer();
         computer.charging(new VoltageAdapter2(new Voltage220V()));
